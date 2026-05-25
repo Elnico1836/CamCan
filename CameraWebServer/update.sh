89c34acf3d6e500a3b96582914f84ead6356b114
Elnico1836 <150050367+Elnico1836@users.noreply.github.com>
2026-05-24 18:33:22 -0700

    Agregar update

---
+++ CameraWebServer/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+sudo cp ~/CamCan/CameraWebServer/cancamera-flask.service /etc/systemd/system/
+sudo cp ~/CamCan/CameraWebServer/cancamera-kiosk.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable cancamera-flask cancamera-kiosk
+sudo systemctl restart cancamera-flask
+sleep 5
+curl http://localhost:5000/health
