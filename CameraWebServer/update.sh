aee340db7d487c68ef00c47208ee225e66c71c96
Elnico1836 <150050367+Elnico1836@users.noreply.github.com>
2026-05-24 18:52:19 -0700

    Update update.sh

--- CameraWebServer/update.sh
+++ CameraWebServer/update.sh
@@ -5,7 +5,10 @@ sudo cp ~/CamCan/CameraWebServer/cancamera-kiosk.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable cancamera-flask cancamera-kiosk
 sudo systemctl set-default graphical.target
+mkdir -p ~/.config/lxsession/LXDE-pi
 cp ~/CamCan/CameraWebServer/autostart ~/.config/lxsession/LXDE-pi/autostart
+sudo mkdir -p /etc/xdg/lxsession/rpd-x
+sudo cp ~/CamCan/CameraWebServer/autostart /etc/xdg/lxsession/rpd-x/autostart
 sudo systemctl restart cancamera-flask
 sleep 5
 curl http://localhost:5000/health
