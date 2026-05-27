95bbe7c7b07bacc649de8f94988e59b65534d2da
Elnico1836 <150050367+Elnico1836@users.noreply.github.com>
2026-05-26 21:39:26 -0500

    Cambios Modelo Keras

--- CameraWebServer/python convertir.py
+++ CameraWebServer/python convertir.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
 
-# Carga con custom_objects para ignorar renorm
 class FixedBatchNorm(tf.keras.layers.BatchNormalization):
     def __init__(self, **kwargs):
         kwargs.pop('renorm', None)
@@ -9,10 +8,18 @@ class FixedBatchNorm(tf.keras.layers.BatchNormalization):
         kwargs.pop('renorm_momentum', None)
         super().__init__(**kwargs)
 
+class FixedDense(tf.keras.layers.Dense):
+    def __init__(self, **kwargs):
+        kwargs.pop('quantization_config', None)
+        super().__init__(**kwargs)
+
 old_model = tf.keras.models.load_model(
     "clasificador_canecas.h5",
     compile=False,
-    custom_objects={"BatchNormalization": FixedBatchNorm}
+    custom_objects={
+        "BatchNormalization": FixedBatchNorm,
+        "Dense": FixedDense
+    }
 )
 
 old_model.save("clasificador_canecas_compat.keras")
